1237240c7bc7a11f2dbdf97ec73d599e408abe98
Saleher Begum <58544434+Saleher@users.noreply.github.com>
2024-07-25 06:51:58 +0000

    dkjf

---
+++ react-essential/src/App.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+import Header from './components/Header/Header.jsx';
+import CoreConcepts from './components/CoreConcepts.js';
+import Example from './components/Example.jsx';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <CoreConcepts />
+        <Example />
+      </main>
+    </>
+  );
+}
+
+export default App;
